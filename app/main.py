12617ad7b1a90c39bc85b0503d16a57e28cfc75e
kimnanhee <kimnanhee0225@gmail.com>
2021-09-17 15:03:12 +0900

    [Modify] main.py

--- app/main.py
+++ app/main.py
@@ -2,13 +2,24 @@ from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 from db.session import engine
 from db.base import Base
+from apis.base import api_router
+from webapp.base import api_router as webapp_router
+
 
 def create_tables():
     Base.metadata.create_all(bind=engine)
 
+
+def include_router(app):
+    app.include_router(api_router)
+    app.include_router(webapp_router)
+
+
 def start_application():
-    app = FastAPI()
+    app = FastAPI(title="NH recommand", version="0.0.1")
     create_tables()
+    include_router(app)
     return app
+
 
 app = start_application()
