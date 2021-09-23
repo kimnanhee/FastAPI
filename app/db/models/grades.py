1cf112b8d5b2c7def674514ef931692e5c811f64
kimnanhee <kimnanhee0225@gmail.com>
2021-09-23 16:47:13 +0900

    [Modify] grades models

--- app/db/models/grades.py
+++ app/db/models/grades.py
@@ -1,6 +1,5 @@
 import datetime
-from decimal import Decimal
-from sqlalchemy import Column, Integer, Numeric, String, DateTime
+from sqlalchemy import Column, Integer, DateTime
 
 from db.base_class import Base
 
@@ -8,5 +7,5 @@ class Grade(Base):
     grade_no = Column(Integer, primary_key=True, autoincrement=True)
     user_no = Column(Integer)
     book_no = Column(Integer)
-    grade = Column(Numeric)
+    grade = Column(Integer)
     createtime = Column(DateTime, default=datetime.datetime.now())
