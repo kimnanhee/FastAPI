678d397688d819cb91f99bdfb089e3ca1de6afe5
kimnanhee <kimnanhee0225@gmail.com>
2021-09-23 16:53:43 +0900

    [Modify] apis base.py

--- app/apis/base.py
+++ app/apis/base.py
@@ -1,7 +1,13 @@
 from fastapi import APIRouter
 
+from apis.version1 import route_users
 from apis.version1 import route_books
+from apis.version1 import route_login
+from apis.version1 import route_grades
 
 api_router = APIRouter()
 
+api_router.include_router(route_users.router, prefix="/users", tags=["users"])
 api_router.include_router(route_books.router, prefix="/books", tags=["books"])
+api_router.include_router(route_login.router, prefix="/login", tags=["login"])
+api_router.include_router(route_grades.router, prefix="/grades", tags=["grades"])
