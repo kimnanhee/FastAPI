c89046d649c34c265d17e6ad8cef3aa1b89d4d51
kimnanhee <kimnanhee0225@gmail.com>
2021-09-24 17:30:01 +0900

    [Modify] webapp base.py, add users router

--- app/webapp/base.py
+++ app/webapp/base.py
@@ -1,7 +1,9 @@
 from fastapi import APIRouter
 
 from webapp.books import route_books
+from webapp.users import route_users
 
 api_router = APIRouter()
 
 api_router.include_router(route_books.router, prefix="", tags=["homepage"])
+api_router.include_router(route_users.router, prefix="", tags=["users"])
