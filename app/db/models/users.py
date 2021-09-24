f24d00125a2f8c5c62db2db25f740120a1467ad9
kimnanhee <kimnanhee0225@gmail.com>
2021-09-24 17:28:48 +0900

    [Modify] db\models users.py

--- app/db/models/users.py
+++ app/db/models/users.py
@@ -5,6 +5,6 @@ from db.base_class import Base
 
 class User(Base):
     user_no = Column(Integer, primary_key=True, autoincrement=True)
-    id = Column(String(50))
+    id = Column(String(50), unique=True)
     pw = Column(String(50))
     createtime = Column(DateTime, default=datetime.datetime.now())
