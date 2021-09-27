a420ea506b484c46e8f12b994b0026be202d2d42
kimnanhee <kimnanhee0225@gmail.com>
2021-09-27 17:27:22 +0900

    [Modify] webapp\books route_books.py, search function

--- app/webapp/books/route_books.py
+++ app/webapp/books/route_books.py
@@ -1,8 +1,9 @@
 from fastapi import APIRouter, Request, Depends, responses, status
 from fastapi.templating import Jinja2Templates
 from sqlalchemy.orm import Session
+from typing import Optional
 from db.session import get_db
-from db.repository.books import list_books, retreive_book
+from db.repository.books import list_books, list_books_name, retreive_book
 
 from webapp.books.forms import BookCreateForm
 from schemas.books import CreateBook
@@ -16,7 +17,7 @@ router = APIRouter(include_in_schema=False)
 @router.get("/")
 def home(request : Request, db : Session = Depends(get_db), msg : str = None):
     books = list_books(db = db)
-    return templates.TemplateResponse("books/homepage.html", {"request": request, "jobs": books, "msg": msg})
+    return templates.TemplateResponse("books/homepage.html", {"request": request, "books": books, "msg": msg})
 
 
 @router.get("/detail/{book_no}")
@@ -49,3 +50,9 @@ async def create_job(request: Request, db: Session=Depends(get_db)):
 def show_books_to_delete(request: Request, db: Session=Depends(get_db)):
     books = list_books(db=db)
     return templates.TemplateResponse("books/show_books_to_delete.html", {"request": request, "books": books})
+
+
+@router.get("/search")
+def search(request: Request, db: Session=Depends(get_db), q: Optional[str]=None):
+    books = list_books_name(title=q, db=db)
+    return templates.TemplateResponse("books/show_books_to_search.html", {"request": request, "books": books})
