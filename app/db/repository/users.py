d52743e9f7bff68c0282267a900cbcf6c0741aea
kimnanhee <kimnanhee0225@gmail.com>
2021-09-28 12:11:40 +0900

    [Modify] db\repository users.py, add hashed_pw

--- app/db/repository/users.py
+++ app/db/repository/users.py
@@ -2,9 +2,10 @@ from sqlalchemy.orm import Session
 
 from schemas.users import CreateUser
 from db.models.users import User
+from core.hashing import Hasher
 
 def create_new_user(user : CreateUser, db : Session):
-    user = User(id=user.id, pw=user.pw)
+    user = User(id=user.id, pw=user.pw, hashed_pw=Hasher.get_password_hash(user.pw))
     db.add(user)
     db.commit()
     db.refresh(user)
