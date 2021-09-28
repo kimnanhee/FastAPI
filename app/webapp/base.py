8ba78cf9623bf6553188e20e917a7f1f4a8ffd3d
kimnanhee <kimnanhee0225@gmail.com>
2021-09-28 11:51:03 +0900

    [Modify] webapp base.py, add auth api router

--- app/webapp/base.py
+++ app/webapp/base.py
@@ -2,8 +2,10 @@ from fastapi import APIRouter
 
 from webapp.books import route_books
 from webapp.users import route_users
+from webapp.auth import route_login
 
 api_router = APIRouter()
 
 api_router.include_router(route_books.router, prefix="", tags=["homepage"])
 api_router.include_router(route_users.router, prefix="", tags=["users"])
+api_router.include_router(route_login.router, prefix="", tags=["auth"])
